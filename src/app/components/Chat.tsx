cea0db87fe61ab948786f3347771dd8c5bd4d25a
Ahsan Bilal <ahsanbilal@Ahsans-MacBook-Air.local>
2024-04-08 20:48:21 +0500

    Chat setup

--- src/app/components/Chat.tsx
+++ src/app/components/Chat.tsx
@@ -2,6 +2,7 @@
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '@radix-ui/react-accordion'
 import { FC } from 'react'
 import ChatHeader from './ChatHeader';
+import ChatInput from './ChatInput';
 
 
 
@@ -9,7 +10,7 @@ import ChatHeader from './ChatHeader';
 const Chat: FC = () => {
   return <Accordion type='single' collapsible className='relative mt-3 bg-white z-40 shadow'>
   <AccordionItem value='item-1'>
-      <div className='fixed right-8 w-80 b-8 bg-white border border-gray-200 rounded-md overflow:hidden'>
+      <div className='fixed right-8 bottom-8 w-80 b-8 bg-white border border-gray-200 rounded-md overflow:hidden'>
           <div className='w-full h-full flex flex-col'>
             <AccordionTrigger className='px-6 border-b border-zinc-800'>
                 <ChatHeader></ChatHeader>
@@ -17,6 +18,9 @@ const Chat: FC = () => {
             <AccordionContent>
                 <div className='flex flex-col h-80'>
                     messages
+                    <ChatInput className='px-4'>
+
+                    </ChatInput>
                 </div>
             </AccordionContent>
           </div>
